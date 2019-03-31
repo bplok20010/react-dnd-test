5b663711011a242ad8bb68582407b2ce8f63aa8e
bplok20010 <nobo.zhou@foxmail.com>
2019-03-31 23:15:12 +0800

    git add

---
+++ examples/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+import './style/index.scss';
+import './style/animate.scss';
+
+import Demo from './Demo';
+
+ReactDOM.render(<Demo />, demo);
